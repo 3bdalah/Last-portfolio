0eba23b288efc15c6bd6f953a7c42654df3a548c
abdallllaa <abdoabdalla763@gmail.com>
2021-02-21 03:47:12 +0200

    edited my photo in section about

--- src/components/About.js
+++ src/components/About.js
@@ -8,17 +8,17 @@ const About = () => {
             <div className="container">
               <Title title={"about"}/>
                 <div className="row">
-                <div className="col-lg-6 col-md-6 col-sm-12 m-auto">
-                    <div className="description">
-                  
-                        <p className="desc-content">I'm a software Engineer who is passionate about creating new things, And i'm really big fan to linux.and I care about performance, code efficiency, and the user experience Some technologies I enjoy working with include Javascript, ReactJS, And other <strong>frontend </strong>stuff🤓. </p>
-                    </div>
-                   </div>
-                   <div className="col-lg-4 col-md-6 col-sm-12">
+                <div className="col-lg-3 col-md-4 col-sm-12">
                     <div className="img">
                         <img src={Me} alt="my-img"/>
                     </div>
                    </div>
+                <div className="col-lg-9 col-md-8 col-sm-12 m-auto">
+                    <div className="description">
+                        <p className="desc-content">I'm a software Engineer who is passionate about creating new things, And i'm really big fan to linux.and I care about performance, code efficiency, and the user experience Some technologies I enjoy working with include Javascript, ReactJS, And other <strong>frontend </strong>stuff🤓. </p>
+                    </div>
+                   </div>
+                 
                    
                 </div>
             </div>
