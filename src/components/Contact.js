90ae06fb23b58db8f72725657eb087f964da7acb
abdallllaa <abdoabdalla763@gmail.com>
2021-04-10 07:24:08 +0200

    changed email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
             <div className="container">
                  <Title title="Contact Me" />
                  <div className="content-contact text-center">
-                     <h6 className="mail">abdallah.div@gmail.com</h6>
+                     <h6 className="mail">3bdallah.dev@gmail.com</h6>
                      <p className="mobile">01156448493</p>
                      <a href="https://github.com/abdallllaa/my-resum/raw/master/My%20resume%20.pdf" target="blank" className="resume">resume</a>
                       <br/>
